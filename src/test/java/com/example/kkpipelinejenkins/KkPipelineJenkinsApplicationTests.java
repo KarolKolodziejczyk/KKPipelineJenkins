fdcf4b79adca8e1f406f934877c12940bb745075
Student <kk303201@student.polsl.pl>
2024-05-13 13:08:17 +0200

    Initial commit

---
+++ src/test/java/com/example/kkpipelinejenkins/KkPipelineJenkinsApplicationTests.java
@@ -0,0 +1,36 @@
+package com.example.kkpipelinejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class KkPipelineJenkinsApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testShouldReturnDefaultMessage( ) throws  Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello NIE Jenkins"));
+	}
+
+
+}
+
